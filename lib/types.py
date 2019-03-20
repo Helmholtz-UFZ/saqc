3e8f207140dae294d429c104fffec9aba0dcc126
David Schaefer <david.schaefer@ufz.de>
2019-03-20 16:07:01 +0100

    type hinting module

---
+++ lib/types.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import TypeVar
+
+import numpy as np
+import pandas as pd
+
+T = TypeVar("T")
+ArrayLike = TypeVar("ArrayLike", np.ndarray, pd.Series, pd.DataFrame)
