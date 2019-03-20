7836db9b71d9a878fe23fb3f231e8e7ec2c2e9dc
David Schaefer <david.schaefer@ufz.de>
2019-03-20 17:03:17 +0100

    new flagger

--- flagger/__init__.py
+++ flagger/__init__.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .simpleflagger import SimpleFlagger
 from .dmpflagger import DmpFlagger
 from .positionalflagger import PositionalFlagger
 from .abstractflagger import AbstractFlagger
