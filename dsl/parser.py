d772d7e0c53cffe4d63053a7f1c8ecb58087d942
Bert Palm <bert.palm@ufz.de>
2019-04-05 11:25:38 +0200

    make independent from debian libyaml-dev package

--- dsl/parser.py
+++ dsl/parser.py
@@ -3,8 +3,15 @@
 
 import yaml
 
+
 def parseFlag(expr):
-    content = yaml.load("[{:}]".format(expr), Loader=yaml.CLoader)
+    try:
+        # CLoader needs the debian package libyaml-dev, if it is
+        # not present fall back on default loader.
+        from yaml import CSafeLoader as SafeLoader
+    except ImportError:
+        from yaml import SafeLoader as SafeLoader
+    content = yaml.load("[{:}]".format(expr), Loader=SafeLoader)
     name = content[0]
     out = {}
     for pdict in content[1:]:
