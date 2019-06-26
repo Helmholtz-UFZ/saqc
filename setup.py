11c164d69c689373016f2b88b46f9859e2cd2d30
Bert Palm <bert.palm@ufz.de>
2019-06-26 14:31:14 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import subprocess
+
+import setuptools
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saqc",
+    version=subprocess.check_output('git describe --tags --always'.split()).decode().strip(),
+    author="Bert Palm, David Schaefer, Peter Luenenschloss, Lennard Schmidt",
+    author_email="bert.palm@ufz.de, david.schaefer@ufz.de, peter.luenenschloss@ufz.de, lennart.schmidt@ufz.de",
+    description="automated quality assurance and control tool",
+    long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://git.ufz.de/rdm/saqc",
+    packages=['saqc'],
+    license='RDM Team - UFZ',
+)
