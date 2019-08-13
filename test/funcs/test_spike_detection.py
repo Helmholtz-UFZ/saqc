fe122ec9223cc95a17d38629ea90a2046b8bb6d3
luenensc <peter.luenenschloss@ufz.de>
2019-08-13 14:59:59 +0200

    added pytest-test for spike detection function

--- test/funcs/test_spike_detection.py
+++ test/funcs/test_spike_detection.py
@@ -6,10 +6,18 @@ import numpy as np
 import pandas as pd
 
 from saqc.flagger.baseflagger import BaseFlagger
-from saqc.funcs.spike_detection import flagSpikes_SpektrumBased
+from saqc.flagger.dmpflagger import DmpFlagger
+from saqc.flagger.simpleflagger import SimpleFlagger
 
+from saqc.funcs.spike_detection import flagSpikes_SpektrumBased, flagMad
 
-# @pytest.fixture(scope='module')
+TESTFLAGGERS = [
+    BaseFlagger(['NIL', 'GOOD', 'BAD']),
+    # DmpFlagger(),
+    SimpleFlagger()
+]
+
+@pytest.fixture(scope='module')
 def spiky_data():
     index = pd.date_range(start='1.1.2011', end='2.1.2011', freq='5min')
     spiky_series = pd.Series(np.linspace(1, 2, index.size), index=index, name='spiky_data')
@@ -19,14 +27,10 @@ def spiky_data():
     return spiky_series, flag_assertion
 
 
-def test_flagSpikes_SpektrumBased(spiky_data):
-    flagger = BaseFlagger(['NIL', 'GOOD', 'BAD'])
+@pytest.mark.parametrize('flagger', TESTFLAGGERS)
+def test_flagSpikes_SpektrumBased(spiky_data, flagger):
     data = spiky_data[0]
     flags = flagger.initFlags(data)
     data, flag_result = flagSpikes_SpektrumBased(data, flags, 'spiky_data', flagger)
-
-if __name__ == '__main__':
-    flagger = BaseFlagger(['NIL', 'GOOD', 'BAD'])
-    data = spiky_data()[0]
-    flags = flagger.initFlags(data)
-    data, flag_result = flagSpikes_SpektrumBased(data, flags, 'spiky_data', flagger, diff_method='savgol')
+    test_sum = (flag_result[spiky_data[1]] == flagger.BAD).sum()
+    assert test_sum == len(spiky_data[1])
