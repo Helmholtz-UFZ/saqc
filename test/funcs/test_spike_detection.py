44282e7e3d76f98b7107b9b1d4bc1bc556d06217
Peter Luenenschloss <peter.luenenschloss@ufz.de>
2019-08-18 12:57:21 +0200

    test-tests now cover over all the flagger

--- test/funcs/test_spike_detection.py
+++ test/funcs/test_spike_detection.py
@@ -11,9 +11,11 @@ from saqc.flagger.simpleflagger import SimpleFlagger
 
 from saqc.funcs.spike_detection import flagSpikes_SpektrumBased, flagMad
 
+from saqc.lib.tools import getPandasData
+
 TESTFLAGGERS = [
     BaseFlagger(['NIL', 'GOOD', 'BAD']),
-    # DmpFlagger(),
+    DmpFlagger(),
     SimpleFlagger()]
 
 
@@ -32,14 +34,17 @@ def test_flagSpikes_SpektrumBased(spiky_data, flagger):
     data = spiky_data[0]
     flags = flagger.initFlags(data)
     data, flag_result = flagSpikes_SpektrumBased(data, flags, 'spiky_data', flagger)
+    flag_result = getPandasData(flag_result, 0)
     test_sum = (flag_result[spiky_data[1]] == flagger.BAD).sum()
     assert test_sum == len(spiky_data[1])
+
 
 @pytest.mark.parametrize('flagger', TESTFLAGGERS)
 def test_flagMad(spiky_data, flagger):
     data = spiky_data[0]
     flags = flagger.initFlags(data)
     data, flag_result = flagMad(data, flags, 'spiky_data', flagger, '1H')
+    flag_result = getPandasData(flag_result, 0)
     test_sum = (flag_result[spiky_data[1]] == flagger.BAD).sum()
     assert test_sum == len(spiky_data[1])
 
