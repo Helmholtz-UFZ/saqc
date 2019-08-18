a2c5c69e70e2990e6e3cd86e690db7fb8c44c514
luenensc <peter.luenenschloss@ufz.de>
2019-08-18 15:13:56 +0200

    break test test added / narrowed testing data sets date range

--- test/funcs/test_break_detection.py
+++ test/funcs/test_break_detection.py
@@ -9,7 +9,7 @@ from saqc.flagger.baseflagger import BaseFlagger
 from saqc.flagger.dmpflagger import DmpFlagger
 from saqc.flagger.simpleflagger import SimpleFlagger
 
-from saqc.funcs.constants_detection import flagConstants_VarianceBased
+from saqc.funcs.break_detection import flagBreaks_SpektrumBased
 
 from saqc.lib.tools import getPandasData
 
@@ -20,24 +20,22 @@ TESTFLAGGERS = [
 
 
 @pytest.fixture(scope='module')
-def constants_data():
+def break_data():
     index = pd.date_range(start='1.1.2011 00:00:00', end='1.1.2011 00:03:00', freq='5min')
-    constants_series = pd.Series(np.linspace(-50, 50, index.size), index=index, name='constants_data')
-    constants_series.iloc[5:25] = 0
-    # constants_series.iloc[1000] = -100
+    break_series = pd.Series(np.linspace(0, 1, index.size), index=index, name='break_data')
+    break_series.iloc[5:15] = +100
     flag_assertion = list(range(5, 25))
-    return constants_series, flag_assertion
+    return break_series, flag_assertion
 
 
 @pytest.mark.parametrize('flagger', TESTFLAGGERS)
-def test_flagConstants_VarianceBased(constants_data, flagger):
-    data = constants_data[0]
+def test_flagBreaks_SpektrumBased(break_data, flagger):
+    data = break_data[0]
     flags = flagger.initFlags(data)
-    data, flag_result = flagConstants_VarianceBased(data, flags, 'constants_data', flagger, plateau_window_min='1h')
+    data, flag_result = flagBreaks_SpektrumBased(data, flags, 'break_data', flagger, plateau_window_min='1h')
     flag_result = getPandasData(flag_result, 0)
-    test_sum = (flag_result[constants_data[1]] == flagger.BAD).sum()
-    assert test_sum == len(constants_data[1])
-
+    test_sum = (flag_result[break_data[1]] == flagger.BAD).sum()
+    assert test_sum == len(break_data[1])
 
 
 
