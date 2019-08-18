2769e13153903fa43314010fabd942d810e0a739
luenensc <peter.luenenschloss@ufz.de>
2019-08-18 15:43:08 +0200

    small fixes

--- test/funcs/test_break_detection.py
+++ test/funcs/test_break_detection.py
@@ -21,10 +21,10 @@ TESTFLAGGERS = [
 
 @pytest.fixture(scope='module')
 def break_data():
-    index = pd.date_range(start='1.1.2011 00:00:00', end='1.1.2011 03:00:00', freq='5min')
+    index = pd.date_range(start='1.1.2011 00:00:00', end='1.2.2011 03:00:00', freq='5min')
     break_series = pd.Series(np.linspace(0, 1, index.size), index=index, name='break_data')
     break_series.iloc[5:15] = +100
-    flag_assertion = list(range(5, 25))
+    flag_assertion = [5, 15]
     return break_series, flag_assertion
 
 
@@ -32,14 +32,17 @@ def break_data():
 def test_flagBreaks_SpektrumBased(break_data, flagger):
     data = break_data[0]
     flags = flagger.initFlags(data)
-    data, flag_result = flagBreaks_SpektrumBased(data, flags, 'break_data', flagger, plateau_window_min='1h',
-                                                 first_der_window_size='1h', filter_window_size='1h')
+    data, flag_result = flagBreaks_SpektrumBased(data, flags, 'break_data', flagger)
     flag_result = getPandasData(flag_result, 0)
     test_sum = (flag_result[break_data[1]] == flagger.BAD).sum()
     assert test_sum == len(break_data[1])
 
 
-
+if __name__ == "__main__":
+    flagger = DmpFlagger()
+    data = break_data()[0]
+    flags = flagger.initFlags(data)
+    data, flag_result = flagBreaks_SpektrumBased(data, flags, 'break_data', flagger)
 
 
 
