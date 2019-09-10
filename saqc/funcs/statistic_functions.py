4201810c321a607bb9e168bad507eea035b6849c
Peter Luenenschloss <peter.luenenschloss@ufz.de>
2019-09-10 13:13:17 +0200

    simplified and boosted nan-control wrapper (still slow)

--- saqc/funcs/statistic_functions.py
+++ saqc/funcs/statistic_functions.py
@@ -6,15 +6,15 @@ import numpy as np
 
 
 def _is_valid(data, max_nan_total, max_nan_consec):
-    if max_nan_total is None:
+    if (max_nan_total is np.inf) & (max_nan_consec is np.inf):
         return True
 
     nan_mask = data.isna()
 
     if nan_mask.sum() <= max_nan_total:
-        if max_nan_consec is None:
+        if max_nan_consec is np.inf:
             return True
-        elif ((1-(~nan_mask)).groupby((~nan_mask).cumsum()).transform(pd.Series.cumsum)).max() <= max_nan_consec:
+        elif nan_mask.rolling(window=max_nan_consec+1).sum().max() <= max_nan_consec:
             return True
         else:
             return False
@@ -22,7 +22,7 @@ def _is_valid(data, max_nan_total, max_nan_consec):
         return False
 
 
-def std_qc(data, max_nan_total=None, max_nan_consec=None):
+def std_qc(data, max_nan_total=np.inf, max_nan_consec=np.inf):
     """Pandas built in function for statistical moments have quite poor nan- control, so here comes a wrapper that
     will return the standart deviation for a given series input, if the total number of nans in the series does
     not exceed "max_nan_total" and the number of consecutive nans does not exceed max_nan_consec.
@@ -36,7 +36,7 @@ def std_qc(data, max_nan_total=None, max_nan_consec=None):
     return np.nan
 
 
-def var_qc(data, max_nan_total=None, max_nan_consec=None):
+def var_qc(data, max_nan_total=np.inf, max_nan_consec=np.inf):
     """Pandas built in function for statistical moments have quite poor nan- control, so here comes a wrapper that
     will return the variance for a given series input, if the total number of nans in the series does
     not exceed "max_nan_total" and the number of consecutive nans does not exceed max_nan_consec.
@@ -45,12 +45,12 @@ def var_qc(data, max_nan_total=None, max_nan_consec=None):
     :param max_nan_total    Integer. Number of np.nan entries allowed to be contained in the series
     :param max_nan_consec   Integer. Maximal number of consecutive nan entries allowed to occure in data.
     """
-    if _is_valid(data, max_nan_total, max_nan_consec):
+    if _is_valid(data, max_nan_total=np.inf, max_nan_consec=np.inf):
         return data.var()
     return np.nan
 
 
-def mean_qc(data, max_nan_total=None, max_nan_consec=None):
+def mean_qc(data, max_nan_total=np.inf, max_nan_consec=np.inf):
     """Pandas built in function for statistical moments have quite poor nan- control, so here comes a wrapper that
     will return the mean for a given series input, if the total number of nans in the series does
     not exceed "max_nan_total" and the number of consecutive nans does not exceed max_nan_consec.
@@ -62,3 +62,5 @@ def mean_qc(data, max_nan_total=None, max_nan_consec=None):
     if _is_valid(data, max_nan_total, max_nan_consec):
         return data.mean()
     return np.nan
+
+
