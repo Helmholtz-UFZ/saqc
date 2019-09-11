b65f0ea48e7650e9a193bb4cd5335d52482327dd
Bert Palm <bert.palm@ufz.de>
2019-09-11 14:55:14 +0200

    fixed some imports

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+from .common import *
+from .test_core import *
+from .flagger import *
+from .dsl import *
+from .funcs import *
--- test/init.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .common import *
-from .test_core import *
-from .dsl.test_generic import *
-from .dsl.test_evaluator import *
-from .flagger.test_dmpflagger import *
