4698b8e49a90a4920c6dfc9dd8a54a70e2982878
Peter Luenenschloss <peter.luenenschloss@ufz.de>
2019-11-27 17:58:40 +0100

    Update test_break_detection.py

--- test/funcs/test_break_detection.py
+++ test/funcs/test_break_detection.py
@@ -8,7 +8,7 @@ from saqc.funcs.break_detection import flagBreaks_spektrumBased
 from test.common import TESTFLAGGER, initData
 
 
-#@pytest.fixture
+@pytest.fixture
 def data():
     return initData(
         1, start_date="2011-01-01 00:00:00", end_date="2011-01-02 03:00:00", freq="5min"
