da85993c6cc4c7612dcd0e9fda72575d9f495d96
David Schaefer <david.schaefer@ufz.de>
2019-12-17 02:46:39 +0100

    changes to the cli

--- saqc/__main__.py
+++ saqc/__main__.py
@@ -9,19 +9,21 @@ import pandas as pd
 from saqc.core import runner
 from saqc.flagger import CategoricalFlagger
 
+
 FLAGGERS = {
     "number": CategoricalFlagger([-1, 0, 1]),
-    "string": CategoricalFlagger(["NIL", "OK", "BAD"])
+    "category": CategoricalFlagger(["NIL", "OK", "BAD"])
 }
+
 
 @click.command()
 @click.option("-c", "--config", type=click.Path(exists=True), required=True, help="path to the configuration file")
 @click.option("-d", "--data", type=click.Path(exists=True), required=True, help="path to the data file")
-@click.option("--flagger", default="string", type=click.Choice(FLAGGERS.keys()), help="the flagging scheme to use")
+@click.option("-o", "--outfile", type=click.Path(), help="path to the output file")
+@click.option("--flagger", default="category", type=click.Choice(FLAGGERS.keys()), help="the flagging scheme to use")
+@click.option("--nodata", default=np.nan, help="nodata value")
 @click.option("--fail/--no-fail", default=True, help="whether to stop the program run on errors")
-@click.option("--nodata", default=np.nan, help="the nodata value")
-@click.option("--plot/--noplot", default=False, help="whether to plot the results")
-def main(config, data, flagger, fail, nodata, plot):
+def main(config, data, flagger, outfile, nodata, fail):
 
     data = pd.read_csv(
         data,
