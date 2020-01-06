f5ceeb31aa69bbd7407a98a402cf0907bab138e7
David Schaefer <david.schaefer@ufz.de>
2020-01-06 02:26:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,32 @@
-import subprocess
-
 import setuptools
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="saqc",
-    version=subprocess.check_output("git describe --tags --always".split())
-    .decode()
-    .strip(),
+    version="1.0.0",
     author="Bert Palm, David Schaefer, Peter Luenenschloss, Lennard Schmidt",
     author_email="bert.palm@ufz.de, david.schaefer@ufz.de, peter.luenenschloss@ufz.de, lennart.schmidt@ufz.de",
-    description="automated quality assurance and control tool",
+    description="Data quality checking and processing framework",
     long_description=long_description,
-    # long_description_content_type="text/markdown",
-    url="https://git.ufz.de/rdm/saqc",
+    long_description_content_type="text/markdown",
+    url="https://git.ufz.de/rdm-software/saqc",
     packages=["saqc"],
-    install_requires=["pandas", "numpy", "numba", "PyYAML", "matplotlib"],
-    license="RDM Team - UFZ",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn==0.21.2",
+        "numba",
+        "matplotlib",
+        "click",
+        "pyarrow",
+        "python-intervals",
+    ],
+    license="GPLv3",
+    entry_points = {
+        'console_scripts': ['saqc=saqc.__main__:main'],
+    }
 )
