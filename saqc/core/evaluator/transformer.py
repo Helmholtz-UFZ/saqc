637aa33e63369c8a52f3be670b601e03428978eb
David Schaefer <david.schaefer@ufz.de>
2020-02-21 09:24:31 +0100

    refactoring to less class global state in DslTransformer

--- saqc/core/evaluator/transformer.py
+++ saqc/core/evaluator/transformer.py
@@ -8,40 +8,55 @@ from contextlib import contextmanager
 
 from saqc.core.config import Params
 
-
 class DslTransformer(ast.NodeTransformer):
     def __init__(self, environment: Dict[str, Any]):
         self.environment = environment
         self.arguments = set()
-        self.lookup = True
-
-    @contextmanager
-    def doLookup(self, value):
-        self.lookup = value
-        yield
-        self.lookup = True
 
     def visit_Call(self, node):
-        with self.doLookup(node.func.id not in self.environment["nolookup"]):
-            node = self.generic_visit(
-                ast.Call(func=node.func, args=node.args, keywords=[])
-            )
-        return node
+        new_args = node.args
+        for a in new_args:
+            a.lookup = node.func.id not in self.environment["nolookup"]
+
+        node = ast.Call(func=node.func, args=new_args, keywords=[])
+        return self.generic_visit(node)
 
     def visit_Name(self, node):
+
+        # NOTE:
+        #
+        # There are different categories of name nodes:
+        #
+        # 1. Names that need a lookup in the global/local eval
+        #    environment (e.g. function names, dsl constants, ...)
+        #    -> nodes need to leave visit_Name unaltered
+        # 2. Names that need a lookup in the 'data' DataFrame
+        #    -> nodes need to be rewritten int ast.Subscript
+        # 3. Names that should be treated as constants and be passed to
+        #    functions requiring a 'field' parameter (e.g. 'isflagged')
+        #    -> nodes need to be rewritten to ast.Constant/ast.Str
+        #
+        # TODO:
+        #
+        # The differentiation between these categories is done based
+        # on the two variables out of 'self.environment', namely
+        # 'nolookup' and 'variables' in two different methods
+        # ('vsisit_Call' and 'visit_Name'). This continues to feel hacky
+        # and I really like to see a cleaner solution for that problem
+
         name = node.id
 
         if name == "this":
             name = self.environment["field"]
 
-        if name in self.environment["variables"]:
-            if self.lookup:
+        if name in self.environment["data"]:
+            if getattr(node, "lookup", True):
                 value = ast.Constant(value=name)
                 node = ast.Subscript(
                     value=ast.Name(id="data", ctx=ast.Load()), slice=ast.Index(value=value), ctx=ast.Load(),
                 )
             else:
-                node = ast.Str(s=name)
+                node = ast.Constant(value=name)
 
         self.arguments.add(name)
         return node
