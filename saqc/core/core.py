c455fdc096ef05be7588c2cabc7585088ad56e49
Bert Palm <bert.palm@ufz.de>
2020-06-10 23:51:14 +0200

    improved SaQC.getResult()

--- saqc/core/core.py
+++ saqc/core/core.py
@@ -119,7 +119,21 @@ class SaQC:
             out = out._wrap(func, lineno=lineno, expr=expr)(**kwargs)
         return out
 
-    def getResult(self):
+    def getResult(self, write_back=False):
+        """ Do the actual calculations and return the results.
+
+        Parameters
+        ----------
+        write_back: bool, default False
+            If False, every call will recalculate, eventually plot and return the result anew.
+            If True the resulting data is written back in the SaQC object itself, like if
+            the object would have been initialized with it. Further calls will then directly
+            return the result with no recalculation needed, but a replotting is not possible.
+
+        Returns
+        -------
+        data, flagger: (DictOfSeries, DictOfSeries)
+        """
         data, flagger = self._data, self._flagger
 
         for field, func in self._to_call:
@@ -142,6 +156,11 @@ class SaQC:
 
         if any([func.plot for _, func in self._to_call]):
             plotAllHook(data, flagger)
+
+        if write_back:
+            self._data = data
+            self._flagger = flagger
+            self._to_call = []
 
         return data, flagger
 
