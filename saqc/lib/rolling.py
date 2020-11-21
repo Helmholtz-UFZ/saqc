6324ba0cca2658fdefba482bb96760c7069d8921
Bert Palm <bert.palm@ufz.de>
2020-11-21 14:56:00 +0100

    bugfixes

--- saqc/lib/rolling.py
+++ saqc/lib/rolling.py
@@ -20,14 +20,24 @@ class _CustomBaseIndexer(BaseIndexer):
     expanding: bool
     skip: np.array
     step: int
+    min_periods: int
+    variable_window = None
 
-    def __init__(self, index_array, window_size, forward=False, expanding=False, step=None, mask=None):
-        super().__init__(index_array, window_size, forward=forward, expanding=expanding, step=step, skip=mask)
+    def __init__(self, index_array, window_size, min_periods=None,
+                 forward=False, expanding=False, step=None, mask=None):
+        super().__init__(index_array, window_size, min_periods=min_periods,
+                         forward=forward, expanding=expanding, step=step, skip=mask)
         self.validate()
 
     def validate(self):
-        if not (isinstance(self.window_size, int) and self.window_size > 0):
+        if not (is_integer(self.window_size) and self.window_size > 0):
             raise TypeError('window_size must be positive integer')
+
+        if self.min_periods is None:
+            self.min_periods = 1 if self.variable_window else self.window_size
+        if not (is_integer(self.min_periods) and self.min_periods >= 0):
+            raise TypeError('min_periods must be non negative integer.')
+
         if self.step is not None and not is_integer(self.step):
             raise TypeError('step must be integer.')
         if self.step == 0:
@@ -41,21 +51,32 @@ class _CustomBaseIndexer(BaseIndexer):
                 raise TypeError('mask must have boolean values only.')
             self.skip = ~skip
 
+    def _asserts(self, num_values, min_periods, center, closed):
+        if self.variable_window:
+            assert center is False
+        else:
+            assert closed in [None, 'right']
+
     def get_window_bounds(self, num_values=0, min_periods=None, center=False, closed=None):
 
-        # only for consistency. min_periods is None if and only
-        # if we have a fixed window and the user did not passed it.
-        # Nevertheless min_periods is not used so far.
-        if min_periods is None:
-            min_periods = self.window_size
+        self._asserts(num_values, min_periods, center, closed)
+        min_periods = self.min_periods
+
+        new_length = num_values
+        offset = calculate_center_offset(self.window_size)
+        new_length = num_values + offset if center else num_values
+        print(locals())
 
         if self.forward:
-            start, end = self._fw(num_values, min_periods, center, closed)
+            start, end = self._fw(new_length, min_periods, center, closed)
         else:
-            start, end = self._bw(num_values, min_periods, center, closed)
+            start, end = self._bw(new_length, min_periods, center, closed)
+
+        if center:
+            start, end = self._center_result(start, end, offset)
 
         if not self.expanding:
-            start, end = self._remove_ramp(start, end)
+            start, end = self._remove_ramps(start, end, num_values, center, min_periods)
 
         if self.skip is not None:
             start, end = self._apply_skipmask(start, end)
@@ -63,6 +84,20 @@ class _CustomBaseIndexer(BaseIndexer):
         if self.step is not None:
             start, end = self._apply_steps(start, end, num_values)
 
+        start, end = self._mask_min_periods(start, end, num_values)
+
+        return start, end
+
+    def _mask_min_periods(self, start, end, num_values):
+        end[end > num_values] = num_values
+        m = end - start < self.min_periods
+        end[m] = 0
+        return start, end
+
+    def _center_result(self, start, end, offset):
+        if offset > 0:
+            start = start[offset:]
+            end = end[offset:]
         return start, end
 
     def _apply_skipmask(self, start, end):
@@ -76,29 +111,8 @@ class _CustomBaseIndexer(BaseIndexer):
         end[m] = 0
         return start, end
 
-    def _remove_ramp(self, start, end, center=False):
-        fw, bw = self.forward, not self.forward
-        winsz = self.window_size
-        if center:
-            winsz //= 2
-
-        if center or fw:
-            # remove (up) ramp
-            # we dont want this: [1,1,1,1,1].rolling(window='2min', forward=True).sum() -> [3, 3, 3,  2,  1  ]
-            # instead we want:   [1,1,1,1,1].rolling(window='2min', forward=True).sum() -> [3, 3, 3, nan, nan]
-            tresh = self.index_array[-1] - winsz
-            mask = self.index_array > tresh
-            end[mask] = 0
-
-        if center or bw:
-            # remove (down) ramp
-            # we dont want this: [1,1,1,1,1].rolling(window='2min').sum() -> [1,   2,   3, 3, 3]
-            # instead we want:   [1,1,1,1,1].rolling(window='2min').sum() -> [nan, nan, 3, 3, 3]
-            tresh = self.index_array[0] + winsz
-            mask = self.index_array < tresh
-            end[mask] = 0
-
-        return start, end
+    def _remove_ramps(self, start, end, num_values, center, min_periods):
+        raise NotImplementedError
 
     def _bw(self, num_values=0, min_periods=None, center=False, closed=None):
         raise NotImplementedError
@@ -111,18 +125,26 @@ class FixedWindowDirectionIndexer(_CustomBaseIndexer):
     # automatically added in super call to init
     index_array: np.array
     window_size: int
+    variable_window = False
 
-    def _raise_if_closed(self, closed):
-        if closed is None or closed == 'right':
-            return
-        raise ValueError(f"closed other than 'right' is not implemented for fixed windows.")
+    def _remove_ramps(self, start, end, num_values, center, min_periods):
+        fw, bw = self.forward, not self.forward
+        offset_l = offset_r = calculate_center_offset(num_values)
+        if center:
+            fw = bw = True
+            offset_l = (offset_l + 1) // 2
+            offset_r = (offset_r + 0) // 2
+
+        offset_l = min(min_periods, offset_l)
+        offset_r = min(min_periods, offset_r)
+        if bw:
+            end[-offset_l:] = 0
+        if fw:
+            end[:offset_r] = 0
+
+        return start, end
 
     def _bw(self, num_values=0, min_periods=None, center=False, closed=None):
-        self._raise_if_closed(closed)
-
-        offset = calculate_center_offset(self.window_size)
-        num_values += offset if center else 0
-
         # code taken from pd.core.windows.indexer.FixedWindowIndexer
         start_s = np.zeros(self.window_size, dtype="int64")
         start_e = (np.arange(self.window_size, num_values, dtype="int64") - self.window_size + 1)
@@ -132,32 +154,15 @@ class FixedWindowDirectionIndexer(_CustomBaseIndexer):
         end_e = start_e + self.window_size
         end = np.concatenate([end_s, end_e])[:num_values]
         # end stolen code
-
-        if center and offset > 0:
-            start = start[offset:]
-            end = end[offset:]
-            end[-offset:] = 0
-
         return start, end
 
     def _fw(self, num_values=0, min_periods=None, center=False, closed=None):
-        self._raise_if_closed(closed)
-
-        offset = calculate_center_offset(self.window_size)
-        num_values += offset if center else 0
-
         # code taken from pd.core.windows.indexer.FixedForwardWindowIndexer
         start = np.arange(num_values, dtype="int64")
         end_s = start[: -self.window_size] + self.window_size
         end_e = np.full(self.window_size, num_values, dtype="int64")
         end = np.concatenate([end_s, end_e])
         # end stolen code
-
-        if center and offset > 0:
-            start = start[:offset]
-            end = end[:offset]
-            end[:offset] = 0
-
         return start, end
 
 
@@ -165,6 +170,24 @@ class VariableWindowDirectionIndexer(_CustomBaseIndexer):
     # automatically added in super call to init
     index_array: np.array
     window_size: int
+    variable_window = True
+
+    def _remove_ramps(self, start, end, num_values, center, min_periods):
+        if self.forward:
+            # remove (up) ramp
+            # we dont want this: [1,1,1,1,1].rolling(window='2min', forward=True).sum() -> [3, 3, 3,  2,  1  ]
+            # instead we want:   [1,1,1,1,1].rolling(window='2min', forward=True).sum() -> [3, 3, 3, nan, nan]
+            tresh = self.index_array[-1] - self.window_size
+            mask = self.index_array > tresh
+        else:
+            # remove (down) ramp
+            # we dont want this: [1,1,1,1,1].rolling(window='2min').sum() -> [1,   2,   3, 3, 3]
+            # instead we want:   [1,1,1,1,1].rolling(window='2min').sum() -> [nan, nan, 3, 3, 3]
+            tresh = self.index_array[0] + self.window_size
+            mask = self.index_array < tresh
+
+        end[mask] = 0
+        return start, end
 
     def _bw(self, num_values=0, min_periods=None, center=False, closed=None):
         arr = self.index_array
@@ -260,95 +283,15 @@ def customRoller(obj, window, min_periods=None,  # aka minimum non-nan values
     if expanding is None:
         expanding = not x.is_freq_type
 
-    kwargs = dict(forward=forward, expanding=expanding, step=step, mask=mask)
+    kwargs = dict(forward=forward, expanding=expanding, step=step, mask=mask, min_periods=min_periods)
     if x.is_freq_type:
         window_indexer = VariableWindowDirectionIndexer(x._on.asi8, x.window, **kwargs)
     else:
         window_indexer = FixedWindowDirectionIndexer(x._on.asi8, window, **kwargs)
 
-    return obj.rolling(window_indexer, min_periods=x.min_periods, center=center, on=on, axis=axis, closed=closed)
-
-
-if __name__ == '__main__':
-    s1 = pd.Series(1., index=pd.date_range("1999/12", periods=12, freq='1M') + pd.Timedelta('1d'))
-    s2 = pd.Series(1., index=pd.date_range('2000/05/15', periods=8, freq='1d'))
-    s = pd.concat([s1, s2]).sort_index()
-    s.name = 's'
-    s[15] = np.nan
-
-    r = customRoller(s, 4)
-    r = customRoller(s, '4d')
-
-    df = pd.DataFrame(s)
-    df['32d'] = s.rolling('32d').sum()
-    df['C-32d'] = customRoller(s, '32d').sum()
-    df['32d-mp2'] = s.rolling('32d', min_periods=2).sum()
-    df['C-32d-mp2'] = customRoller(s, '32d', min_periods=2).sum()
-    print('\n', df)
-
-    df = pd.DataFrame(s)
-    df['ffhack-32d'] = pd.Series(reversed(s), reversed(s.index)).rolling('32d').sum()[::-1]
-    df['Cff-32d'] = customRoller(s, '32d', forward=True).sum()
-    df['ffhack-32d-mp2'] = pd.Series(reversed(s), reversed(s.index)).rolling('32d', min_periods=2).sum()[::-1]
-    df['Cff-32d-mp2'] = customRoller(s, '32d', min_periods=2, forward=True).sum()
-    print('\n', df)
-
-    df = pd.DataFrame(s)
-    df['4'] = s.rolling(4).sum()
-    df['C4'] = customRoller(s, 4).sum()
-    df['4-mp2'] = s.rolling(4, min_periods=2).sum()
-    df['C4-mp2'] = customRoller(s, 4, min_periods=2).sum()
-    print('\n', df)
-
-    df = pd.DataFrame(s)
-    df['ffhack4'] = pd.Series(reversed(s), reversed(s.index)).rolling(4).sum()[::-1]
-    df['Cff-4'] = customRoller(s, 4, forward=True).sum()
-    df['ffhack4-mp2'] = pd.Series(reversed(s), reversed(s.index)).rolling(4, min_periods=2).sum()[::-1]
-    df['Cff-4-mp2'] = customRoller(s, 4, min_periods=2, forward=True).sum()
-    df['ffhack4-mp1'] = pd.Series(reversed(s), reversed(s.index)).rolling(4, min_periods=1).sum()[::-1]
-    df['Cff-4-mp1'] = customRoller(s, 4, min_periods=1, forward=True).sum()
-    print('\n', df)
-
-    df = pd.DataFrame(s)
-    mask = [True, False] * int(len(s) / 2)
-    df['m[TF]'] = customRoller(s, 4, mask=mask).sum()
-    df['stp=3'] = customRoller(s, 4, step=3).sum()
-    df['m[TF]stp=3'] = customRoller(s, 4, step=3, mask=mask).sum()
-    df['ff-m[TF]'] = customRoller(s, 4, forward=True, mask=mask).sum()
-    df['ff-stp=3'] = customRoller(s, 4, forward=True, step=3).sum()
-    df['ff-m[TF]stp=3'] = customRoller(s, 4, forward=True, step=3, mask=mask).sum()
-    print('\n', df)
-
-    s = pd.Series(1., index=pd.date_range('2000/05/15', periods=10, freq='1d'))
-    df = pd.DataFrame(s)
-    df['3d-right'] = s.rolling('3d', closed='right').sum()
-    df['C3d-right'] = customRoller(s, '3d', closed='right').sum()
-    df['3d-left'] = s.rolling('3d', closed='left').sum()
-    df['C3d-left'] = customRoller(s, '3d', closed='left').sum()
-    print('\n', df)
-
-    df = pd.DataFrame(s)
-    df['3d-both'] = s.rolling('3d', closed='both').sum()
-    df['C3d-both'] = customRoller(s, '3d', closed='both').sum()
-    df['3d-neither'] = s.rolling('3d', closed='neither').sum()
-    df['C3d-neither'] = customRoller(s, '3d', closed='neither').sum()
-    print('\n', df)
-
-    s[6] = 0
-    df = pd.DataFrame(s)
-    df['a'] = s.rolling(7, center=False).sum()
-    df['a`'] = customRoller(s, 7, center=False).sum()
-
-    df = pd.DataFrame(s)
-    df['Cffc'] = customRoller(s, 8, forward=True, center=True).sum()
-    print('\n', df)
-    # exit(3)
-
-    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]:
-        df = pd.DataFrame(s)
-        print(i, (i - 1) // 2)
-        df['c'] = s.rolling(i, center=True).sum()
-        df['Cc'] = customRoller(s, i, center=True).sum()
-        df['Cffc'] = customRoller(s, i, forward=True, center=True).sum()
-        print('\n', df)
+    # center offset is calculated by min_periods if rolling(indexer)
+    # for normal .rolling(window,...) from window instead
+    # if min_periods == None or 0, all values exist even if start[i]==end[i]
+    # ->> always pass 1
+    return obj.rolling(window_indexer, min_periods=1, center=center, on=on, axis=axis, closed=closed)
 
