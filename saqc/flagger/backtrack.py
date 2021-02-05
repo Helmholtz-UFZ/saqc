bf9fb73206e7779f45af926059da1036f9df3997
Bert Palm <bert.palm@ufz.de>
2021-02-05 15:04:41 +0100

    impl squeeze and other stuff thts useful

--- saqc/flagger/backtrack.py
+++ saqc/flagger/backtrack.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 from __future__ import annotations
 
-from typing import Type, Any, Tuple
+from typing import Tuple
 import pandas as pd
 import numpy as np
-import dios
-
-# fixme: if we decide for a container
-#  check every occurrence of this
 
 
 class Backtrack:
@@ -25,7 +21,7 @@ class Backtrack:
             bt = bt.bt
             mask = bt.mask
         else:
-            self._validate_bt(bt, mask)
+            bt, mask = self._validate_bt(bt, mask)
 
         if copy:
             bt = bt.copy()
@@ -38,10 +34,6 @@ class Backtrack:
     @property
     def nr(self):
         return self._nr
-
-    def next(self):
-        self._nr += 1
-        return self
 
     @property
     def index(self) -> pd.Index:
@@ -94,6 +86,26 @@ class Backtrack:
         self.bt[touched] = False
 
     def _insert(self, value: pd.Series, nr: int, force=False) -> Backtrack:
+        """
+        Insert data at an arbitrary position in the BT.
+
+        No checks.
+
+        Parameters
+        ----------
+        value : pd.Series
+            the series to insert
+
+        nr : int
+            the position to insert
+
+        force : bool, default False
+            if True the internal mask is updated accordingly
+
+        Returns
+        -------
+        Backtrack
+        """
         s = self._validate_value(value)
 
         if s.empty:
@@ -109,6 +121,56 @@ class Backtrack:
         self.mask[nr] = pd.Series(True, index=s.index)
         self.bt[nr] = s
 
+        return self
+
+    def squeeze(self, n: int):
+        """
+        Squeeze last `n` columns to a single column.
+
+        Parameters
+        ----------
+        n : int
+            last n cloumns to squeeze
+
+        Notes
+        -----
+        The new column number (column name) will be the lowest of
+        the squeezed. This ensure that the column numbers are always
+        monotonic increasing.
+
+        Bear in mind, this works inplace, if a copy is needed, call ``copy`` before.
+
+        Returns
+        -------
+        Backtrack: squeezed backtrack
+        """
+        if n == 0:
+            return self
+
+        if n > len(self):
+            raise ValueError(f"'n={n}' cannot be greater than columns in the BT")
+
+        # shortcut
+        if len(self) == n:
+            self.bt = pd.DataFrame()
+            self.mask = pd.DataFrame()
+            s = self.max()
+
+        else:
+            # calc the squeezed series.
+            # we dont have to care about any forced series
+            # because anytime force is given, the False's in
+            # the mask are propagated back over the whole BT
+            mask = self.mask.iloc[:, -n:]
+            bt = self.bt.iloc[: -n:]
+            s = bt[mask].max(axis=1)
+
+            # slice self down
+            self.bt = self.bt.iloc[:, :-n]
+            self.mask = self.mask.iloc[:, :-n]
+
+        self._nr = len(self)
+        self.append(s)
         return self
 
     def append(self, value: pd.Series) -> Backtrack:
@@ -131,7 +193,7 @@ class Backtrack:
         Backtrack: BT with appended series
         """
         self._insert(value, self.nr + 1)
-        self.next()
+        self._nr += 1
         return self
 
     def max(self):
@@ -144,4 +206,8 @@ class Backtrack:
         """
         return self.bt[self.mask].max(axis=1)
 
+    def __len__(self):
+        return len(self.bt)
 
+    def copy(self, deep=True):
+        return Backtrack(bt=self, copy=deep)
