5414a96303ac4b12a8dde8181beb3c5a7b6e8006
Peter Luenenschloss <peter.luenenschloss@ufz.de>
2021-05-11 12:43:45 +0200

    [FIX] fixed flagOffset behavior for the case of signum swaps

--- saqc/funcs/outliers.py
+++ saqc/funcs/outliers.py
@@ -896,7 +896,7 @@ def flagOffset(
 
     if rel_thresh:
         s = np.sign(rel_thresh)
-        rel_jumps = s * (dataseries.shift(1).div(dataseries) - 1) > abs(rel_thresh)
+        rel_jumps = s * (dataseries.shift(1) - dataseries).div(dataseries.abs()) > abs(rel_thresh)
         if thresh:
             post_jumps = rel_jumps & post_jumps
         else:
