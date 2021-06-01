8a0ba2c8f25f4ca0bf06a979183eb850cdc27a9a
Peter Luenenschloss <peter.luenenschloss@ufz.de>
2021-06-01 19:40:52 +0200

    some saqc constants doc

--- saqc/constants.py
+++ saqc/constants.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+The module comprises flag value constants in use throughout saqc.
+The constants order as follows (from "worse" to "best"):
+
+:py:const:`~saqc.constants.BAD` > :py:const:`~saqc.constants.DOUBTFUL` > :py:const:`~saqc.constants.GOOD` >
+:py:const:`~saqc.constants.UNFLAGGED` (> :py:const:`~saqc.constants.UNTOUCHED`)
+"""
 
 __all__ = [
     "UNTOUCHED",
@@ -11,11 +18,25 @@ __all__ = [
 
 import numpy as np
 
+#: Internal :py:mod:`flag level constant <saqc.constants>`.
+#: When returned by a test, it indicates, that the test did not consider to flag the respective value
 UNTOUCHED = np.nan
+
+#: A :py:mod:`flag level constant <saqc.constants>`
+#: , evaluating to the level, that indicates, no flag has been assigned to yet.
 UNFLAGGED = -np.inf
+
+#: A :py:mod:`flag level constant <saqc.constants>`
+#: , evaluating to the lowest level level of flagging, that is
+#: not :py:const:`UNFLAGGED <saqc.constants.UNFLAGGED>`.
 GOOD = 0
+
+#: A :py:mod:`flag level constant <saqc.constants>`
+#: , evaluating to a somewhat modest flag level.
 DOUBTFUL = 25.0
+
+#: A :py:mod:`flag level constant <saqc.constants>`
+#: , evaluating to the highest (internal) flag level available.
 BAD = 255.0
 
-# aliases
-DOUBT = DOUBTFUL
+DOUBT = DOUBTFUL  #: Alias for :py:const:`DOUBTFUL <saqc.constants.DOUBTFUL>`.
