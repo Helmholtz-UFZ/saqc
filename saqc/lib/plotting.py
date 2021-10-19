8faa75277ac0ba3482cfe156b98c34db1fdc6319
luenensc <peter.luenenschloss@ufz.de>
2021-10-19 13:18:44 +0200

    black

--- saqc/lib/plotting.py
+++ saqc/lib/plotting.py
@@ -23,8 +23,7 @@ STATSDICT = {
     "flagged percentage": lambda x, y, z: round(((y >= z).sum()) / len(x), 2),
 }
 
-PLOT_KWARGS = {"alpha": 0.8,
-               "linewidth": 1}
+PLOT_KWARGS = {"alpha": 0.8, "linewidth": 1}
 AX_KWARGS = {}
 FIG_KWARGS = {"figsize": (16, 9)}
 SCATTER_KWARGS = {
