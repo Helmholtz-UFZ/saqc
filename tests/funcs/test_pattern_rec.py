a050fb7f9bced8e25a081e43ad9134b71e82e236
David Schaefer <david.schaefer@ufz.de>
2021-11-24 17:53:59 +0100

    remove wavelet function

--- tests/funcs/test_pattern_rec.py
+++ tests/funcs/test_pattern_rec.py
@@ -5,9 +5,9 @@ import pytest
 import pandas as pd
 import dios
 
-from saqc.constants import *
+from saqc.constants import BAD, UNFLAGGED
 from saqc.core import initFlagsLike
-from saqc.funcs.pattern import *
+from saqc.funcs.pattern import flagPatternByDTW
 from tests.common import initData
 
 
@@ -19,23 +19,6 @@ def data():
 @pytest.fixture
 def field(data):
     return data.columns[0]
-
-
-@pytest.mark.skip(reason="faulty implementation - wait for #GL216")
-def test_flagPattern_wavelet():
-    data = pd.Series(0, index=pd.date_range(start="2000", end="2001", freq="1d"))
-    data.iloc[10:18] = [0, 5, 6, 7, 6, 8, 5, 0]
-    pattern = data.iloc[10:18]
-
-    data = dios.DictOfSeries(dict(data=data, pattern_data=pattern))
-    flags = initFlagsLike(data, name="data")
-    data, flags = flagPatternByWavelet(
-        data, "data", flags, reference="pattern_data", flag=BAD
-    )
-
-    assert all(flags["data"].iloc[10:18] == BAD)
-    assert all(flags["data"].iloc[:9] == UNFLAGGED)
-    assert all(flags["data"].iloc[18:] == UNFLAGGED)
 
 
 def test_flagPattern_dtw():
@@ -52,7 +35,3 @@ def test_flagPattern_dtw():
     assert all(flags["data"].iloc[10:18] == BAD)
     assert all(flags["data"].iloc[:9] == UNFLAGGED)
     assert all(flags["data"].iloc[18:] == UNFLAGGED)
-
-    # visualize:
-    # data['data'].plot()
-    # ((flags['data']>0) *5.).plot()
