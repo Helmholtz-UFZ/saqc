4a44561066c575a2230d4faf2e1bf76534f6b1e8
luenensc <peter.luenenschloss@ufz.de>
2022-02-03 11:45:20 +0100

    added licence notes

--- sphinxdoc/scripts/templates.py
+++ sphinxdoc/scripts/templates.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021 Helmholtz-Zentrum für Umweltforschung GmbH - UFZ
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import re
 
 FUNC_NAPOLEAN_STYLE_ORDER = ['Head', 'Parameters', 'Returns', 'Notes', 'See also', 'Examples', 'References']
