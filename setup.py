d698e8d2d8c7cd41d9a491d7464952d82715e082
Bert Palm <bert.palm@ufz.de>
2023-06-20 11:50:47 +0200

    pandas>=2.0.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "numpy",
         "outlier-utils",
         "pyarrow",
-        "pandas",
+        "pandas>=2.0.0",
         "scikit-learn",
         "scipy",
         "typing_extensions",
