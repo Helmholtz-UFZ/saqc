d15f215920e1ce83f80b3f64bfce2a9574deb62a
Bert Palm <bert.palm@ufz.de>
2023-07-27 12:46:33 +0200

    setup versioneer

--- saqc/version.py
+++ saqc/version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
 # SPDX-FileCopyrightText: 2021 Helmholtz-Zentrum für Umweltforschung GmbH - UFZ
-#
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-__version__ = "2.4.1"
+from . import _version
+
+__version__ = _version.get_versions()["version"]
